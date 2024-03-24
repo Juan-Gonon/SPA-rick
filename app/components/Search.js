767b5524492b07a18109d0733e58e1c128f772fb
Juan Gonon <juanfegonon27@gmail.com>
2024-03-23 23:28:30 -0600

    add styles to finder

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,6 +1,7 @@
 export const search = ()=>{
     let input = document.createElement('input');
-    input.classList.add('search')
+    input.classList.add('search');
+    input.setAttribute('placeholder', 'Busca');
     input.id = "buscador";
     input.style.display = "none";
 
